62bc9eb412ff8ae57b046ec58a95144380df23c1
khairulhaaziq <rhycoz@yahoo.com>
2023-07-18 05:36:03 +0800

    lint

--- packages/shadcn-vue/src/components/uiv2/cva.ts
+++ packages/shadcn-vue/src/components/uiv2/cva.ts
@@ -11,16 +11,30 @@ export const AccordionClass = cva("", {
 			header: "",
 			trigger: "",
 			content: "",
-      contentWrapper: "",
+			contentWrapper: "",
 		},
 	},
 	compoundVariants: [
 		{ variant: "default", component: "root", class: "w-[300px]" },
 		{ variant: "default", component: "item", class: "border-b w-full" },
-    { variant: "default", component: "trigger", class: "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180" },
-    { variant: "default", component: "content", class: "overflow-hidden text-sm data-[state=open]:animate-accordion-down data-[state=closed]:animate-accordion-up" },
-    { variant: "default", component: "contentWrapper", class: "overflow-hidden text-sm" },
-    { variant: "default", component: "header", class: "flex" },
+		{
+			variant: "default",
+			component: "trigger",
+			class:
+				"flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+		},
+		{
+			variant: "default",
+			component: "content",
+			class:
+				"overflow-hidden text-sm data-[state=open]:animate-accordion-down data-[state=closed]:animate-accordion-up",
+		},
+		{
+			variant: "default",
+			component: "contentWrapper",
+			class: "overflow-hidden text-sm",
+		},
+		{ variant: "default", component: "header", class: "flex" },
 	],
 	defaultVariants: {
 		variant: "default",
