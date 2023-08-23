d9416e81f870ea4cc733c0701999580d665c1bf2
zernonia <59365435+zernonia@users.noreply.github.com>
2023-08-23 15:10:31 +0800

    feat: popover (#6)
    * feat: add popover demo
    
    * chore: rename parseEmit, fix tsconfig baseURL missing

--- packages/shadcn-vue/src/App.vue
+++ packages/shadcn-vue/src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import AccordionDemo from './components/ui/AccordionDemo.vue'
+import PopoverDemo from './components/ui/PopoverDemo.vue'
 </script>
 
 <template>
-  <div class="p-8">
+  <div class="p-8 grid gap-8 grid-cols-3 place-items-center">
     <AccordionDemo class="max-w-[20rem]" />
+
+    <PopoverDemo />
   </div>
 </template>
---
+++ packages/shadcn-vue/src/components/ui/PopoverDemo.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from './Popover'
+</script>
+
+<template>
+  <Popover>
+    <PopoverTrigger as-child>
+      <button>
+        Open popover
+      </button>
+    </PopoverTrigger>
+    <PopoverContent class="w-80" @open-auto-focus.prevent>
+      <div class="grid gap-4">
+        <div class="space-y-2">
+          <h4 class="font-medium leading-none">
+            Dimensions
+          </h4>
+          <p class="text-sm text-muted-foreground">
+            Set the dimensions for the layer.
+          </p>
+        </div>
+        <div class="grid gap-2">
+          <div class="grid grid-cols-3 items-center gap-4">
+            <label for="width">Width</label>
+            <input
+              id="width"
+              type="text"
+              default-value="100%"
+              class="col-span-2 h-8"
+            >
+          </div>
+          <div class="grid grid-cols-3 items-center gap-4">
+            <label for="maxWidth">Max. width</label>
+            <input
+              id="maxWidth"
+              type="text"
+              default-value="300px"
+              class="col-span-2 h-8"
+            >
+          </div>
+          <div class="grid grid-cols-3 items-center gap-4">
+            <label for="height">Height</label>
+            <input
+              id="height"
+              type="text"
+              default-value="25px"
+              class="col-span-2 h-8"
+            >
+          </div>
+          <div class="grid grid-cols-3 items-center gap-4">
+            <label for="maxHeight">Max. height</label>
+            <input
+              id="maxHeight"
+              type="text"
+              default-value="none"
+              class="col-span-2 h-8"
+            >
+          </div>
+        </div>
+      </div>
+    </PopoverContent>
+  </Popover>
+</template>
