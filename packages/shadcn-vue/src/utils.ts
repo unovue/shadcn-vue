2eb1131e03e3d06c1b3aba423b09e07c94396a60
zernonia <zernonia@gmail.com>
2023-08-23 15:06:14 +0800

    chore: rename parseEmit, fix tsconfig baseURL missing

--- packages/shadcn-vue/src/utils.ts
+++ packages/shadcn-vue/src/utils.ts
@@ -2,7 +2,7 @@ import type { ClassValue } from 'clsx'
 import { clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-export type ShortEmits<T extends Record<string, any>> = {
+export type ParseEmits<T extends Record<string, any>> = {
   [K in keyof T]: (...args: T[K]) => void;
 }
 
