dd48dd8132cf2625b57051e4953c02440d1f0163
zernonia <zernonia@gmail.com>
2023-08-30 09:28:05 +0800

    chore: add docs

---
+++ docs/.vitepress/config.mts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "shadcn-vue",
+  description: "A VitePress Site"
+})
---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,11 @@
+// https://vitepress.dev/guide/custom-theme
+import Layout from './Layout.vue'
+import './style.css'
+
+export default {
+  Layout,
+  enhanceApp({ app, router, siteData }) {
+    // ...
+  }
+}
+
