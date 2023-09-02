309c87a293ef68faa07bfaa53cfee41240b0e6e9
Ahmed Mayara <99325011+ahmedmayara@users.noreply.github.com>
2023-09-02 15:36:45 +0100

    [Feature]: Sheet (#23)
    * fix: fix alert dialog missing emits
    
    * feat: add sheet component

---
+++ apps/www/src/lib/registry/default/ui/sheet/SheetDescription.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { DialogDescription, type DialogDescriptionProps } from 'radix-vue'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<DialogDescriptionProps & { class?: string }>()
+</script>
+
+<template>
+  <DialogDescription
+    :class="cn('text-sm text-muted-foreground', props.class)"
+    v-bind="props"
+  >
+    <slot />
+  </DialogDescription>
+</template>
---
+++ apps/www/src/lib/registry/default/ui/sheet/SheetHeader.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils'
+
+const props = defineProps<{ class?: string }>()
+</script>
+
+<template>
+  <div
+    :class="
+      cn('flex flex-col space-y-2 text-center sm:text-left', props.class)
+    "
+  >
+    <slot />
+  </div>
+</template>
