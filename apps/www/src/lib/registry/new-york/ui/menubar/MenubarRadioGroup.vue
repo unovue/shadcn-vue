0768fa5bdd512b02ef4fafa049fb261f22d96159
Ahmed <ahmedmayara789@gmail.com>
2023-09-06 14:42:40 +0100

    feat: add menubar

---
+++ apps/www/src/lib/registry/new-york/ui/menubar/MenubarRadioGroup.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import {
+  MenubarRadioGroup,
+  type MenubarRadioGroupEmits,
+  type MenubarRadioGroupProps,
+} from 'radix-vue'
+
+const props = defineProps<MenubarRadioGroupProps>()
+
+const emits = defineEmits<MenubarRadioGroupEmits>()
+</script>
+
+<template>
+  <MenubarRadioGroup
+    v-bind="props"
+    @update:model-value="emits('update:modelValue', $event)"
+  >
+    <slot />
+  </MenubarRadioGroup>
+</template>
---
+++ apps/www/src/lib/registry/new-york/ui/menubar/MenubarSubTrigger.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { MenubarSubTrigger, type MenubarSubTriggerProps } from 'radix-vue'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<MenubarSubTriggerProps & { inset?: boolean; class?: string }>()
+</script>
+
+<template>
+  <MenubarSubTrigger
+    v-bind="props"
+    :class="[
+      cn(
+        'flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground',
+        inset && 'pl-8',
+        props.class,
+      ),
+    ]"
+  >
+    <slot />
+  </MenubarSubTrigger>
+</template>
