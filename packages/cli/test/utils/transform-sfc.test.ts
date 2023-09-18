0b39d050b320bd6c91673e9066eded7214eacde0
zernonia <zernonia@gmail.com>
2023-09-18 22:38:37 +0800

    chore: cleanup

--- packages/cli/test/utils/transform-sfc.test.ts
+++ packages/cli/test/utils/transform-sfc.test.ts
@@ -1,15 +1,23 @@
-import { readFileSync } from 'node:fs'
-import { resolve } from 'node:path'
 import { describe, expect, test } from 'vitest'
 import { transform } from '../../src/utils/transformers'
 
 describe('transformSFC', () => {
-  const appVuePath = resolve(__dirname, '../fixtures/transform-sfc/app.vue')
-  const content = readFileSync(appVuePath, 'utf-8')
   test('basic', async () => {
     const result = await transform({
       filename: 'app.vue',
-      raw: content,
+      raw: `<script lang="ts" setup>
+      const array: (number | string)[] = [1, 2, 3]
+      </script>
+      
+      <template>
+        <div v-bind="{ array }">
+          template
+        </div>
+      </template>
+      
+      <style scoped>
+      </style>
+      `,
       config: {},
     })
     expect(result).toMatchSnapshot()
