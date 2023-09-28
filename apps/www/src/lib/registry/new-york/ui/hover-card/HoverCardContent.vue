b60f22de9067bbed7e691c4d6bafec88d70e5d8b
zernonia <zernonia@gmail.com>
2023-09-28 15:42:04 +0800

    refactor: change all popper related element to use useForwardPropsEmits

--- apps/www/src/lib/registry/new-york/ui/hover-card/HoverCardContent.vue
+++ apps/www/src/lib/registry/new-york/ui/hover-card/HoverCardContent.vue
@@ -3,6 +3,7 @@ import {
   HoverCardContent,
   type HoverCardContentProps,
   HoverCardPortal,
+  useForwardProps,
 } from 'radix-vue'
 import { cn } from '@/lib/utils'
 
@@ -12,12 +13,14 @@ const props = withDefaults(
     sideOffset: 4,
   },
 )
+
+const forwarded = useForwardProps(props)
 </script>
 
 <template>
   <HoverCardPortal>
     <HoverCardContent
-      v-bind="props"
+      v-bind="forwarded"
       :class="
         cn(
           'z-50 w-64 rounded-md bg-background border border-border p-4 text-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
