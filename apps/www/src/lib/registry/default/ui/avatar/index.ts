e0cecc34d63b73275c0e74aada2e42359e3f5f0a
Muhammad Mahmoud <MuhammadM.1097@gmail.com>
2023-10-02 12:01:25 +0300

    fix:  avatar class typo (#97)
    * fix: class typo
    
    * fix: same typo different file

--- apps/www/src/lib/registry/default/ui/avatar/index.ts
+++ apps/www/src/lib/registry/default/ui/avatar/index.ts
@@ -5,7 +5,7 @@ export { default as AvatarImage } from './AvatarImage.vue'
 export { default as AvatarFallback } from './AvatarFallback.vue'
 
 export const avatarVariant = cva(
-  'inline-flex items-center justify-center font-normal text-foregorund select-none shrink-0 bg-secondary overflow-hidden',
+  'inline-flex items-center justify-center font-normal text-foreground select-none shrink-0 bg-secondary overflow-hidden',
   {
     variants: {
       size: {
