e5b33f652fb939b847ef8f11b73c2d8ec6d8eb8a
Sadegh Barati <sadeghbaratiwork@gmail.com>
2023-10-10 10:39:16 +0330

    feat: add `pagination` (#109)
    feat: update

---
+++ apps/www/src/lib/registry/new-york/example/PaginationDemo.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {
+  Pagination,
+  PaginationEllipsis,
+  PaginationFirst,
+  PaginationLast,
+  PaginationList,
+  PaginationListItem,
+  PaginationNext,
+  PaginationPrev,
+} from '@/lib/registry/new-york/ui/pagination'
+
+import {
+  Button,
+} from '@/lib/registry/new-york/ui/button'
+</script>
+
+<template>
+  <Pagination v-slot="{ page }" :total="100" :sibling-count="1" show-edges :default-page="2">
+    <PaginationList v-slot="{ items }" class="flex items-center gap-1">
+      <PaginationFirst />
+      <PaginationPrev />
+
+      <template v-for="(item, index) in items">
+        <PaginationListItem v-if="item.type === 'page'" :key="index" :value="item.value" as-child>
+          <Button class="w-9 h-9 p-0" :variant="item.value === page ? 'default' : 'outline'">
+            {{ item.value }}
+          </Button>
+        </PaginationListItem>
+        <PaginationEllipsis v-else :key="item.type" :index="index" />
+      </template>
+
+      <PaginationNext />
+      <PaginationLast />
+    </PaginationList>
+  </Pagination>
+</template>
