bd622b89c9ec6d98361951879d8a79137ab9e71b
Ole Marius Løset <olemariusloset@gmail.com>
2023-10-13 13:14:19 +0200

    fix: selecttrigger disabled style

--- apps/www/src/lib/registry/default/ui/select/SelectTrigger.vue
+++ apps/www/src/lib/registry/default/ui/select/SelectTrigger.vue
@@ -17,7 +17,7 @@ const props = withDefaults(
     v-bind="props"
     :class="[
       cn(
-        'flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        'flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 data-[disabled]:cursor-not-allowed data-[disabled]:opacity-50',
         props.class,
       ),
       props.invalid
