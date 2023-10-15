116f88274949aca37289a2ef229140973b2deeac
Dunqing <dengqing0821@gmail.com>
2023-10-15 21:18:43 +0800

    fix: teest case

--- packages/cli/test/utils/transform-cjs-to-esm.test.ts
+++ packages/cli/test/utils/transform-cjs-to-esm.test.ts
@@ -3,6 +3,6 @@ import { TAILWIND_CONFIG, TAILWIND_CONFIG_WITH_VARIABLES } from '../../src/utils
 import { transformCJSToESM } from '../../src/utils/transformers/transform-cjs-to-esm'
 
 test('handle tailwind config template correctly', () => {
-  expect(transformCJSToESM('.js', TAILWIND_CONFIG)).toMatchSnapshot()
-  expect(transformCJSToESM('.js', TAILWIND_CONFIG_WITH_VARIABLES)).toMatchSnapshot()
+  expect(transformCJSToESM('.mjs', TAILWIND_CONFIG)).toMatchSnapshot()
+  expect(transformCJSToESM('.mjs', TAILWIND_CONFIG_WITH_VARIABLES)).toMatchSnapshot()
 })
