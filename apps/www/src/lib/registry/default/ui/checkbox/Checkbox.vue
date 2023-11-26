4156bf6703d7e60ec4831dfed777773fc1e6b882
Benjamin Gervan <gben9302@gmail.com>
2023-11-26 10:23:05 +0100

    fix: checkbox icon off-center fix (#184)
    * fix: checkbox icon off-center fix
    
    * fix: checkbox icon off-center update fix
    
    * fix: revert box-content

--- apps/www/src/lib/registry/default/ui/checkbox/Checkbox.vue
+++ apps/www/src/lib/registry/default/ui/checkbox/Checkbox.vue
@@ -17,7 +17,7 @@ const forwarded = useForwardPropsEmits(props, emits)
       cn('peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
          $attrs.class ?? '')"
   >
-    <CheckboxIndicator class="flex items-center justify-center text-current">
+    <CheckboxIndicator class="flex h-full w-full items-center justify-center text-current">
       <Check class="h-4 w-4" />
     </CheckboxIndicator>
   </CheckboxRoot>
