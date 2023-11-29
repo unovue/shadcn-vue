bdf8d31eb07f7addad296b760922ca5b03203753
sadeghbarati <sadeghbaratiwork@gmail.com>
2023-11-29 13:37:11 +0330

    fix: input duplicated class name, remove type="text"
    If the **type** attribute is not specified, the default type is "text"

--- apps/www/src/lib/registry/default/ui/input/Input.vue
+++ apps/www/src/lib/registry/default/ui/input/Input.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
+import { useAttrs } from 'vue'
 import { useVModel } from '@vueuse/core'
 import { cn } from '@/lib/utils'
+
+defineOptions({
+  inheritAttrs: false,
+})
 
 const props = defineProps<{
   defaultValue?: string | number
@@ -11,6 +16,8 @@ const emits = defineEmits<{
   (e: 'update:modelValue', payload: string | number): void
 }>()
 
+const { class: className, ...rest } = useAttrs()
+
 const modelValue = useVModel(props, 'modelValue', emits, {
   passive: true,
   defaultValue: props.defaultValue,
@@ -18,5 +25,5 @@ const modelValue = useVModel(props, 'modelValue', emits, {
 </script>
 
 <template>
-  <input v-model="modelValue" type="text" :class="cn('flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50', $attrs.class ?? '')">
+  <input v-model="modelValue" :class="cn('flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50', className ?? '')" v-bind="rest">
 </template>
