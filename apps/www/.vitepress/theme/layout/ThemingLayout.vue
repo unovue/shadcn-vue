7388347c478c2e6f28cf5c2ccf2c762349439c2d
Josh Larsen <2565382+joshlarsen@users.noreply.github.com>
2023-12-01 16:32:27 -0500

    fix tooltip bg color

--- apps/www/.vitepress/theme/layout/ThemingLayout.vue
+++ apps/www/.vitepress/theme/layout/ThemingLayout.vue
@@ -117,7 +117,7 @@ watch(radius, (radius) => {
                   <TooltipContent
                     align="center"
                     :side-offset="1"
-                    class="capitalize"
+                    class="capitalize bg-zinc-900"
                   >
                     {{ allColors[index] }}
                   </TooltipContent>
