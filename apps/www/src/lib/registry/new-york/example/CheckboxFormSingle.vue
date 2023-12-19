8cae5b70b5b32e74179789c53ab14807ea8f2592
Sadegh Barati <sadeghbaratiwork@gmail.com>
2023-12-19 18:03:46 +0330

    docs: fix checkbox and radio form examples
    add type="checkbox|radio" and name in FormField

--- apps/www/src/lib/registry/new-york/example/CheckboxFormSingle.vue
+++ apps/www/src/lib/registry/new-york/example/CheckboxFormSingle.vue
@@ -37,7 +37,7 @@ const onSubmit = handleSubmit((values) => {
 
 <template>
   <form class="space-y-6" @submit="onSubmit">
-    <FormField v-slot="{ value, handleChange }" name="mobile">
+    <FormField v-slot="{ value, handleChange }" type="checkbox" name="mobile">
       <FormItem class="flex flex-row items-start gap-x-3 space-y-0 rounded-md border p-4 shadow">
         <FormControl>
           <Checkbox :checked="value" @update:checked="handleChange" />
