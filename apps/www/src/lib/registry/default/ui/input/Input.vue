fa0cae16b6c52d78cbec12d63a6157ae7a70fc3f
parasSolanki <parassolanki151@gmail.com>
2023-12-29 20:24:46 +0530

    fix: default input props not reactive

--- apps/www/src/lib/registry/default/ui/input/Input.vue
+++ apps/www/src/lib/registry/default/ui/input/Input.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useAttrs } from 'vue'
+import { computed, useAttrs } from 'vue'
 import { useVModel } from '@vueuse/core'
 import { cn } from '@/lib/utils'
 
@@ -16,7 +16,13 @@ const emits = defineEmits<{
   (e: 'update:modelValue', payload: string | number): void
 }>()
 
-const { class: className, ...rest } = useAttrs()
+const rawAttrs = useAttrs()
+
+const attrs = computed(() => {
+  const { class: className, ...rest } = rawAttrs
+
+  return { className, rest }
+})
 
 const modelValue = useVModel(props, 'modelValue', emits, {
   passive: true,
@@ -25,5 +31,5 @@ const modelValue = useVModel(props, 'modelValue', emits, {
 </script>
 
 <template>
-  <input v-model="modelValue" :class="cn('flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50', className ?? '')" v-bind="rest">
+  <input v-model="modelValue" :class="cn('flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50', attrs.className ?? '')" v-bind="attrs.rest">
 </template>
