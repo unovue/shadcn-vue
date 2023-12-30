c650c14c08fcb6f7d7120e14d0bef8a28a5bfd8a
itsTPM <26815728+itsTPM@users.noreply.github.com>
2023-12-30 16:53:37 +0300

    docs: change cards radius to `rounded-lg`

--- apps/www/.vitepress/theme/components/LinkedCard.vue
+++ apps/www/.vitepress/theme/components/LinkedCard.vue
@@ -3,7 +3,7 @@ import { cn } from '@/lib/utils'
 </script>
 
 <template>
-  <a :class="cn('flex w-full flex-col items-center rounded-xl border bg-card p-6 text-card-foreground shadow transition-colors hover:bg-muted/50 sm:p-10', $attrs.class ?? '')">
+  <a :class="cn('flex w-full flex-col items-center rounded-lg border bg-card p-6 text-card-foreground shadow transition-colors hover:bg-muted/50 sm:p-10', $attrs.class ?? '')">
     <slot />
   </a>
 </template>
