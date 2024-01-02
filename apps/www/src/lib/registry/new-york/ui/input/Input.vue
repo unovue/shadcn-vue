3b12a0a29f09b0f577c68242238ffe0dc9202edc
parasSolanki <parassolanki151@gmail.com>
2024-01-02 18:54:12 +0530

    fix: new-york input not reactive added class prop

--- apps/www/src/lib/registry/new-york/ui/input/Input.vue
+++ apps/www/src/lib/registry/new-york/ui/input/Input.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, useAttrs } from 'vue'
+import type { HTMLAttributes } from 'vue'
 import { useVModel } from '@vueuse/core'
 import { cn } from '@/lib/utils'
 
@@ -10,19 +10,12 @@ defineOptions({
 const props = defineProps<{
   defaultValue?: string | number
   modelValue?: string | number
+  class?: HTMLAttributes['class']
 }>()
 
 const emits = defineEmits<{
   (e: 'update:modelValue', payload: string | number): void
 }>()
-
-const rawAttrs = useAttrs()
-
-const attrs = computed(() => {
-  const { class: className, ...rest } = rawAttrs
-
-  return { className, rest }
-})
 
 const modelValue = useVModel(props, 'modelValue', emits, {
   passive: true,
@@ -31,5 +24,5 @@ const modelValue = useVModel(props, 'modelValue', emits, {
 </script>
 
 <template>
-  <input v-model="modelValue" :class="cn('flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50', attrs.className ?? '')" v-bind="attrs.rest">
+  <input v-model="modelValue" :class="cn('flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50', props.class ?? '')" v-bind="$attrs">
 </template>
