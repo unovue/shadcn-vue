0f0f1ef3ca7deea59013d160827e0e2fdcb902f9
Paras Solanki <62164346+ParasSolanki@users.noreply.github.com>
2024-01-03 02:07:30 +0530

    fix: Input `attrs` not reactive (#228)
    * fix: default input props not reactive
    
    * fix: new-york input props are not reactive
    
    * fix: default input not reactive added class prop
    
    * fix: new-york input not reactive added class prop
    
    * fix: update
    
    ---------
    
    Co-authored-by: Sadegh Barati <sadeghbaratiwork@gmail.com>

--- apps/www/src/lib/registry/new-york/ui/input/Input.vue
+++ apps/www/src/lib/registry/new-york/ui/input/Input.vue
@@ -1,22 +1,17 @@
 <script setup lang="ts">
-import { useAttrs } from 'vue'
+import type { HTMLAttributes } from 'vue'
 import { useVModel } from '@vueuse/core'
 import { cn } from '@/lib/utils'
-
-defineOptions({
-  inheritAttrs: false,
-})
 
 const props = defineProps<{
   defaultValue?: string | number
   modelValue?: string | number
+  class?: HTMLAttributes['class']
 }>()
 
 const emits = defineEmits<{
   (e: 'update:modelValue', payload: string | number): void
 }>()
-
-const { class: className, ...rest } = useAttrs()
 
 const modelValue = useVModel(props, 'modelValue', emits, {
   passive: true,
@@ -25,5 +20,5 @@ const modelValue = useVModel(props, 'modelValue', emits, {
 </script>
 
 <template>
-  <input v-model="modelValue" :class="cn('flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50', className ?? '')" v-bind="rest">
+  <input v-model="modelValue" :class="cn('flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50', props.class ?? '')">
 </template>
