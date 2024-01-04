9d9a6f929ce0f281b4af36161af80ed2bbdc4a16
Starker-xp <starker-xp@users.noreply.github.com>
2024-01-04 14:07:25 +0100

    fix: dialog props missing (#242)

--- apps/www/src/lib/registry/default/ui/dialog/Dialog.vue
+++ apps/www/src/lib/registry/default/ui/dialog/Dialog.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-import { DialogRoot } from 'radix-vue'
+import { DialogRoot, type DialogRootEmits, type DialogRootProps, useForwardPropsEmits } from 'radix-vue'
+
+const props = defineProps<DialogRootProps>()
+const emits = defineEmits<DialogRootEmits>()
+
+const forwarded = useForwardPropsEmits(props, emits)
 </script>
 
 <template>
-  <DialogRoot>
+  <DialogRoot v-bind="forwarded">
     <slot />
   </DialogRoot>
 </template>
