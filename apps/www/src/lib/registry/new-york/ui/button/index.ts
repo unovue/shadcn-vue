ff2727500c16ab3ddc10732acf322b49672da852
hrynevych.romann <hrynevych.romann@gmail.com>
2024-01-11 22:47:33 +0200

    fix(Button): add 'whitespace-nowrap' to base styles

--- apps/www/src/lib/registry/new-york/ui/button/index.ts
+++ apps/www/src/lib/registry/new-york/ui/button/index.ts
@@ -3,7 +3,7 @@ import { cva } from 'class-variance-authority'
 export { default as Button } from './Button.vue'
 
 export const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center rounded-md whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
