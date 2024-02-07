c5bae2b84af40d0bf3033facf7e597e18e7655d9
zernonia <zernonia@gmail.com>
2024-02-07 20:11:51 +0800

    chore: improve tag highlight

--- apps/www/src/lib/registry/new-york/ui/tags-input/TagsInputItem.vue
+++ apps/www/src/lib/registry/new-york/ui/tags-input/TagsInputItem.vue
@@ -16,7 +16,7 @@ const forwardedProps = useForwardProps(delegatedProps)
 </script>
 
 <template>
-  <TagsInputItem v-bind="forwardedProps" :class="cn('flex h-5 items-center rounded-md bg-secondary', props.class)">
+  <TagsInputItem v-bind="forwardedProps" :class="cn('flex h-5 items-center rounded-md bg-secondary data-[state=active]:ring-ring data-[state=active]:ring-2 data-[state=active]:ring-offset-2 ring-offset-background', props.class)">
     <slot />
   </TagsInputItem>
 </template>
