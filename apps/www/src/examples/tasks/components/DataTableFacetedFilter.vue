afef9124ac3ec8b7f306361fc0e911f5378178ce
hrynevych.romann <hrynevych.romann@gmail.com>
2024-02-08 23:58:45 +0200

    fix: add type to `term` param in `filter-function` Command prop

--- apps/www/src/examples/tasks/components/DataTableFacetedFilter.vue
+++ apps/www/src/examples/tasks/components/DataTableFacetedFilter.vue
@@ -74,7 +74,7 @@ const selectedValues = computed(() => new Set(props.column?.getFilterValue() as 
     </PopoverTrigger>
     <PopoverContent class="w-[200px] p-0" align="start">
       <Command
-        :filter-function="(list: DataTableFacetedFilter['options'], term) => list.filter(i => i.label.toLowerCase()?.includes(term)) "
+        :filter-function="(list: DataTableFacetedFilter['options'], term: string) => list.filter(i => i.label.toLowerCase()?.includes(term)) "
       >
         <CommandInput :placeholder="title" />
         <CommandList>
