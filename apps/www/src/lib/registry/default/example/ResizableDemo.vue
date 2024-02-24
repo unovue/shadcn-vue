4ffce6c3e7c206662748d51b97ad06bf6b6ad82f
Sadegh Barati <sadeghbaratiwork@gmail.com>
2024-02-25 00:24:15 +0330

    feat: resizable (splitter)

---
+++ apps/www/src/lib/registry/default/example/ResizableDemo.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/lib/registry/default/ui/resizable'
+</script>
+
+<template>
+  <ResizablePanelGroup
+    direction="horizontal"
+    class="max-w-md rounded-lg border"
+  >
+    <ResizablePanel :default-size="50">
+      <div class="flex h-[200px] items-center justify-center p-6">
+        <span class="font-semibold">One</span>
+      </div>
+    </ResizablePanel>
+    <ResizableHandle />
+    <ResizablePanel :default-size="50">
+      <ResizablePanelGroup direction="vertical">
+        <ResizablePanel :default-size="25">
+          <div class="flex h-full items-center justify-center p-6">
+            <span class="font-semibold">Two</span>
+          </div>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel :default-size="75">
+          <div class="flex h-full items-center justify-center p-6">
+            <span class="font-semibold">Three</span>
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </ResizablePanel>
+  </ResizablePanelGroup>
+</template>
