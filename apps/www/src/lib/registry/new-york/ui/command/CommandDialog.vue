e05d7250e8f193ed01a3a4f2257dd401c35ce059
Mael <mael.primet@gmail.com>
2024-03-01 12:55:43 +0100

    Update CommandDialog.vue in new-york

--- apps/www/src/lib/registry/new-york/ui/command/CommandDialog.vue
+++ apps/www/src/lib/registry/new-york/ui/command/CommandDialog.vue
@@ -4,16 +4,24 @@ import type { DialogRootEmits, DialogRootProps } from 'radix-vue'
 import Command from './Command.vue'
 import { Dialog, DialogContent } from '@/lib/registry/new-york/ui/dialog'
 
-const props = defineProps<DialogRootProps>()
+const props = defineProps<
+  DialogRootProps & {
+    filterFunction?: (val: Array<string | any>, term: string) => Array<any>;
+  }
+>();
 const emits = defineEmits<DialogRootEmits>()
 
 const forwarded = useForwardPropsEmits(props, emits)
+const forwardedExceptFilterFunction = computed(() => {
+  const { filterFunction: _, ...rest } = forwarded.value;
+  return rest;
+});
 </script>
 
 <template>
-  <Dialog v-bind="forwarded">
+  <Dialog v-bind="forwardedExceptFilterFunction">
     <DialogContent class="overflow-hidden p-0 shadow-lg">
-      <Command class="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
+      <Command :filter-function="filterFunction" class="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
         <slot />
       </Command>
     </DialogContent>
