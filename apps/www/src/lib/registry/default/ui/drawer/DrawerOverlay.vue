64e2f9c1997fcaad589674615544f0e9ea1658e4
Saeid Zareie <65568529+Saeid-Za@users.noreply.github.com>
2024-03-04 10:04:28 +0330

    feat: `vaul-vue` integration (#374)

---
+++ apps/www/src/lib/registry/default/ui/drawer/DrawerOverlay.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { DrawerOverlay } from 'vaul-vue'
+import type { DialogOverlayProps } from 'radix-vue'
+import { type HtmlHTMLAttributes, computed } from 'vue'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<DialogOverlayProps & { class?: HtmlHTMLAttributes['class'] }>()
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+
+  return delegated
+})
+</script>
+
+<template>
+  <DrawerOverlay v-bind="delegatedProps" :class="cn('fixed inset-0 z-50 bg-black/80', props.class)" />
+</template>
