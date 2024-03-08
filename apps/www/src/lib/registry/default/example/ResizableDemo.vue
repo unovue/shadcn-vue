ff2327b140bd75cf06cfc8d380a0cad78ca54999
Davlatov Shahzod <shahzod@davlatov.space>
2024-03-08 18:43:19 +0300

    fix: resizable demo not working

--- apps/www/src/lib/registry/default/example/ResizableDemo.vue
+++ apps/www/src/lib/registry/default/example/ResizableDemo.vue
@@ -8,24 +8,25 @@ import {
 
 <template>
   <ResizablePanelGroup
+    id="demo-group-1"
     direction="horizontal"
     class="max-w-md rounded-lg border"
   >
-    <ResizablePanel :default-size="50">
+    <ResizablePanel id="demo-panel-1" :default-size="50">
       <div class="flex h-[200px] items-center justify-center p-6">
-        <span class="font-semibold">One</span>
+        <span class="font-semibold">Test</span>
       </div>
     </ResizablePanel>
-    <ResizableHandle />
-    <ResizablePanel :default-size="50">
-      <ResizablePanelGroup direction="vertical">
-        <ResizablePanel :default-size="25">
+    <ResizableHandle id="demo-handle-1" />
+    <ResizablePanel id="demo-panel-2" :default-size="50">
+      <ResizablePanelGroup id="demo-group-2" direction="vertical">
+        <ResizablePanel id="demo-panel-3" :default-size="25">
           <div class="flex h-full items-center justify-center p-6">
             <span class="font-semibold">Two</span>
           </div>
         </ResizablePanel>
-        <ResizableHandle />
-        <ResizablePanel :default-size="75">
+        <ResizableHandle id="demo-handle-2" />
+        <ResizablePanel id="demo-panel-4" :default-size="75">
           <div class="flex h-full items-center justify-center p-6">
             <span class="font-semibold">Three</span>
           </div>
