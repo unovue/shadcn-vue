b2bba49bb774983a7741d34e2f41c86e944bcce3
sadeghbarati <sadeghbaratiwork@gmail.com>
2024-05-09 10:53:55 +0330

    chore: spaces

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -172,7 +172,7 @@ export async function promptForConfig(
     {
       type: 'text',
       name: 'tailwindConfig',
-      message: `Where is your ${highlight('tailwind.config')} located?  ${colors.gray('(this file will be overwritten)')}`,
+      message: `Where is your ${highlight('tailwind.config')} located? ${colors.gray('(this file will be overwritten)')}`,
       initial: (prev, values) => {
         if (defaultConfig?.tailwind.config)
           return defaultConfig?.tailwind.config
