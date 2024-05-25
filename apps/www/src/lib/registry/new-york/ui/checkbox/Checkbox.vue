2bcb3f8af5fb0fd8f888ecd68ff4c3cf4fcfe61c
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-05-25 10:08:33 -0700

    fix: type error on checkbox

--- apps/www/src/lib/registry/new-york/ui/checkbox/Checkbox.vue
+++ apps/www/src/lib/registry/new-york/ui/checkbox/Checkbox.vue
@@ -14,7 +14,7 @@ const delegatedProps = computed(() => {
   return delegated
 })
 
-const forwarded = useForwardPropsEmits(delegatedProps, emits)
+const forwarded = useForwardPropsEmits(delegatedProps.value, emits)
 </script>
 
 <template>
