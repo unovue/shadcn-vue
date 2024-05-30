ae2c3ed737ec3babc77bdb5b1185d5c6cdd21f91
Saeid Zareie <saeid.za98@gmail.com>
2024-05-30 13:34:44 +0330

    chore: tailwind flat config replacement

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,19 @@
 import antfu from '@antfu/eslint-config'
-import { FlatCompat } from '@eslint/eslintrc'
+import tailwind from 'eslint-plugin-tailwindcss'
 
-const compat = new FlatCompat()
-
-const tailwindLintPlugin = compat.config({
-  extends: ['plugin:tailwindcss/recommended'],
-  rules: {
-    // Turned off, because we leveraged arbitary values in CSS classes.
-    'tailwindcss/no-custom-classname': 'off',
-    // Turned off, as has no usage for this repo (https://github.com/francoismassart/eslint-plugin-tailwindcss/blob/HEAD/docs/rules/migration-from-tailwind-2.md).
-    'tailwindcss/migration-from-tailwind-2': 'off',
-    // This could be turned on, but causes a lot of changes in files.
-    'tailwindcss/classnames-order': 'off',
+const tailwindLintPlugin = [
+  ...tailwind.configs['flat/recommended'],
+  {
+    rules: {
+      // Turned off, because we leveraged arbitary values in CSS classes.
+      'tailwindcss/no-custom-classname': 'off',
+      // Turned off, as has no usage for this repo (https://github.com/francoismassart/eslint-plugin-tailwindcss/blob/HEAD/docs/rules/migration-from-tailwind-2.md).
+      'tailwindcss/migration-from-tailwind-2': 'off',
+      // This could be turned on, but causes a lot of changes in files.
+      'tailwindcss/classnames-order': 'off',
+    },
   },
-})
+]
 
 export default antfu(
   {
