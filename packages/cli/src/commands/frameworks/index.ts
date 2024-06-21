984977c85acc96e5263cd823ba192f4cba0fa870
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-06-21 17:05:46 +0300

    refactor: move 'add' command to vue folder

--- packages/cli/src/commands/frameworks/index.ts
+++ packages/cli/src/commands/frameworks/index.ts
@@ -1,9 +1,11 @@
 import loadConfigVue from './vue/config'
 import initVue from './vue/init'
+import addVue from './vue/add'
 
 export default {
   vue: {
     loadConfig: loadConfigVue,
     init: initVue,
+    add: addVue,
   },
 }
