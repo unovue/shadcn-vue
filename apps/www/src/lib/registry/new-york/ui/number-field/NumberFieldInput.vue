eeca60d09b2904e9407d061ade06d568006e6f08
Hendrig Sellik <sellikhendrig@gmail.com>
2024-06-23 21:58:28 +0300

    fix: make `NumberField` more flexible (#613) (#623)

--- apps/www/src/lib/registry/new-york/ui/number-field/NumberFieldInput.vue
+++ apps/www/src/lib/registry/new-york/ui/number-field/NumberFieldInput.vue
@@ -4,5 +4,5 @@ import { cn } from '@/lib/utils'
 </script>
 
 <template>
-  <NumberFieldInput :class="cn('flex h-9 w-full rounded-md border border-input bg-transparent px-10 py-1 text-sm text-center shadow-sm transition-colors placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50')" />
+  <NumberFieldInput data-slot="input" :class="cn('flex h-9 w-full rounded-md border border-input bg-transparent py-1 text-sm text-center shadow-sm transition-colors placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50')" />
 </template>
