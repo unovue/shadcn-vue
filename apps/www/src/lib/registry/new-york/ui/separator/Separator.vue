cdfbd51190b3c42b9052db291e967fe8ca7ccdd0
yali <121391005+yusufalitangoz@users.noreply.github.com>
2024-06-27 08:21:07 +0300

    fix: remove bg-white in separator in dark mode (#634)

--- apps/www/src/lib/registry/new-york/ui/separator/Separator.vue
+++ apps/www/src/lib/registry/new-york/ui/separator/Separator.vue
@@ -29,7 +29,7 @@ const delegatedProps = computed(() => {
       v-if="props.label"
       :class="
         cn(
-          'text-xs text-muted-foreground bg-background absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white flex justify-center items-center',
+          'text-xs text-muted-foreground bg-background absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center',
           props.orientation === 'vertical' ? 'w-[1px] px-1 py-2' : 'h-[1px] py-1 px-2',
         )
       "
