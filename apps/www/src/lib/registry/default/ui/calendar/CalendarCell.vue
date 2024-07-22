6bf5fd3eebd3a504c3e1dfd67151373e18917872
canstand <canstand@outlook.com>
2024-07-22 21:20:47 +0800

    fix(Calendar): data-outside-month should be data-outside-view

--- apps/www/src/lib/registry/default/ui/calendar/CalendarCell.vue
+++ apps/www/src/lib/registry/default/ui/calendar/CalendarCell.vue
@@ -16,7 +16,7 @@ const forwardedProps = useForwardProps(delegatedProps)
 
 <template>
   <CalendarCell
-    :class="cn('relative h-9 w-9 p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([data-selected])]:rounded-md [&:has([data-selected])]:bg-accent [&:has([data-selected][data-outside-month])]:bg-accent/50', props.class)"
+    :class="cn('relative h-9 w-9 p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([data-selected])]:rounded-md [&:has([data-selected])]:bg-accent [&:has([data-selected][data-outside-view])]:bg-accent/50', props.class)"
     v-bind="forwardedProps"
   >
     <slot />
