0919fa47067897dec31334dbd0d45df334cea7f9
NehemiahAklil <nehemiahaklil@gmail.com>
2024-08-20 22:15:38 +0300

    Update Tags with Combobox example to make conent collapsable

--- apps/www/src/lib/registry/default/example/TagsInputComboboxDemo.vue
+++ apps/www/src/lib/registry/default/example/TagsInputComboboxDemo.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
-import { ComboboxAnchor, ComboboxInput, ComboboxPortal, ComboboxRoot } from 'radix-vue'
+import { ComboboxAnchor, ComboboxInput, ComboboxPortal, ComboboxRoot, ComboboxContent  } from 'radix-vue'
 import { CommandEmpty, CommandGroup, CommandItem, CommandList } from '@/lib/registry/default/ui/command'
 import { TagsInput, TagsInputInput, TagsInputItem, TagsInputItemDelete, TagsInputItemText } from '@/lib/registry/default/ui/tags-input'
 
@@ -36,6 +36,7 @@ const filteredFrameworks = computed(() => frameworks.filter(i => !modelValue.val
       </ComboboxAnchor>
 
       <ComboboxPortal>
+        <ComboboxContent>
         <CommandList
           position="popper"
           class="w-[--radix-popper-anchor-width] rounded-md mt-2 border bg-popover text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
@@ -59,6 +60,7 @@ const filteredFrameworks = computed(() => frameworks.filter(i => !modelValue.val
             </CommandItem>
           </CommandGroup>
         </CommandList>
+        </ComboboxContent>
       </ComboboxPortal>
     </ComboboxRoot>
   </TagsInput>
