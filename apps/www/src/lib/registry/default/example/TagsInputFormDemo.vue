1a49cf00605c91eeef7bbd61604a1e38805b708a
romanhrynevych <hrynevych.romann@gmail.com>
2024-08-22 08:52:00 +0300

    feat(tag-input): add `form` example

---
+++ apps/www/src/lib/registry/default/example/TagsInputFormDemo.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { h, ref } from 'vue'
+import { useForm } from 'vee-validate'
+import { toTypedSchema } from '@vee-validate/zod'
+import { z } from 'zod'
+import { TagsInput, TagsInputInput, TagsInputItem, TagsInputItemDelete, TagsInputItemText } from '@/lib/registry/default/ui/tags-input'
+import { Button } from '@/lib/registry/default/ui/button'
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/lib/registry/default/ui/form'
+import { toast } from '@/lib/registry/default/ui/toast'
+
+const modelValue = ref(['Apple', 'Banana'])
+
+const formSchema = toTypedSchema(z.object({
+  fruits: z.array(z.string()).min(1).max(3),
+}))
+
+const { handleSubmit } = useForm({
+  validationSchema: formSchema,
+})
+
+const onSubmit = handleSubmit((values) => {
+  toast({
+    title: 'You submitted the following values:',
+    description: h('pre', { class: 'mt-2 w-[340px] rounded-md bg-slate-950 p-4' }, h('code', { class: 'text-white' }, JSON.stringify(values, null, 2))),
+  })
+})
+</script>
+
+<template>
+  <form class="w-2/3 space-y-6" @submit="onSubmit">
+    <FormField v-slot="{ value }" v-model="modelValue" name="fruits">
+      <FormItem>
+        <FormLabel>Fruits</FormLabel>
+        <FormControl>
+          <TagsInput :model-value="value">
+            <TagsInputItem v-for="item in modelValue" :key="item" :value="item">
+              <TagsInputItemText />
+              <TagsInputItemDelete />
+            </TagsInputItem>
+
+            <TagsInputInput placeholder="Fruits..." />
+          </TagsInput>
+        </FormControl>
+        <FormDescription>
+          Select your favorite fruits.
+        </FormDescription>
+        <FormMessage />
+      </FormItem>
+    </FormField>
+    <Button type="submit">
+      Submit
+    </Button>
+  </form>
+</template>
