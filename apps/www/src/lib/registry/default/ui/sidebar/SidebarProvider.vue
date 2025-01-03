b52ed8c6fdb303a812ef0605d375e5c963c544b8
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2025-01-03 10:06:31 +0100

    Removes text-sidebar-foreground from SidebarProvider.vue

--- apps/www/src/lib/registry/default/ui/sidebar/SidebarProvider.vue
+++ apps/www/src/lib/registry/default/ui/sidebar/SidebarProvider.vue
@@ -71,7 +71,7 @@ provideSidebarContext({
         '--sidebar-width': SIDEBAR_WIDTH,
         '--sidebar-width-icon': SIDEBAR_WIDTH_ICON,
       }"
-      :class="cn('group/sidebar-wrapper flex min-h-svh w-full text-sidebar-foreground has-[[data-variant=inset]]:bg-sidebar', props.class)"
+      :class="cn('group/sidebar-wrapper flex min-h-svh w-full has-[[data-variant=inset]]:bg-sidebar', props.class)"
       v-bind="$attrs"
     >
       <slot />
