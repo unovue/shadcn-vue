57b429c51867b72ff98fc37521a4c4a927381d03
Alex <widesehl@gmail.com>
2025-02-17 21:44:34 +0500

    fix: add peer class to RadioGroupItem

--- apps/www/src/lib/registry/default/ui/radio-group/RadioGroupItem.vue
+++ apps/www/src/lib/registry/default/ui/radio-group/RadioGroupItem.vue
@@ -27,7 +27,7 @@ const forwardedProps = useForwardProps(delegatedProps)
     v-bind="forwardedProps"
     :class="
       cn(
-        'aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        'peer aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
         props.class,
       )
     "
