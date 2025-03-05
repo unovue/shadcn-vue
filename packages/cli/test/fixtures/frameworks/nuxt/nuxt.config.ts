e3756b616612f4e7e1d364255b258311a2661cc7
zernonia <zernonia@gmail.com>
2025-03-05 18:24:45 +0800

    test: include test fixtures

--- packages/cli/test/fixtures/frameworks/nuxt/nuxt.config.ts
+++ packages/cli/test/fixtures/frameworks/nuxt/nuxt.config.ts
@@ -1,7 +1,13 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   components: [
     {
       path: '~/components/ui',
