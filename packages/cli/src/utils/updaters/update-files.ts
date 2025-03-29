fad179077cadf1354408baa4352928f550a2201f
zernonia <zernonia@gmail.com>
2025-03-29 18:32:48 +0800

    fix: overwrite not working

--- packages/cli/src/utils/updaters/update-files.ts
+++ packages/cli/src/utils/updaters/update-files.ts
@@ -107,7 +107,7 @@ export async function updateFiles(
         folderSkipped.set(folderName, false)
       }
 
-      if (!folderSkipped.has(folderName)) {
+      if (!folderSkipped.has(folderName) && !options.overwrite) {
         filesCreatedSpinner.stop()
         const { overwrite } = await prompts({
           type: 'confirm',
