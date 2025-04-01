2f8700fe091880a4a2a426fc425f2fdae99d29da
zernonia <zernonia@gmail.com>
2025-04-01 15:24:48 +0800

    docs: announcement

--- apps/www/.vitepress/theme/components/Announcement.vue
+++ apps/www/.vitepress/theme/components/Announcement.vue
@@ -6,8 +6,9 @@ import { announcementConfig } from '../config/site'
 <template>
   <a
     :href="announcementConfig.link"
-    class="group mb-2 inline-flex items-center px-0.5 text-sm font-medium"
+    class="group mb-2 inline-flex items-center gap-2 px-0.5 text-sm font-medium"
   >
+    <div v-html="announcementConfig.icon" />
     <span class="sm:hidden underline-offset-4 group-hover:underline">{{ announcementConfig.title }}</span>
     <span class="hidden sm:inline underline-offset-4 group-hover:underline">
       {{ announcementConfig.title }}
