911bf2ec23b2e6451db6aa0ec5c57111c252b16c
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2025-04-08 16:44:36 +0200

    fix(command) - props.value is possibly null
    adds nullish coalescing operator to default nullish vaue props to empty string
    
    src/components/ui/command/CommandItem.vue:48:62 - error TS18047: 'props.value' is possibly 'null'.

--- apps/v4/registry/new-york-v4/ui/command/CommandItem.vue
+++ apps/v4/registry/new-york-v4/ui/command/CommandItem.vue
@@ -45,7 +45,7 @@ onMounted(() => {
     return
 
   // textValue to perform filter
-  allItems.value.set(id, currentElement.value.textContent ?? props.value.toString())
+  allItems.value.set(id, currentElement.value.textContent ?? (props.value?.toString() ?? ''))
 
   const groupId = groupContext?.id
   if (groupId) {
