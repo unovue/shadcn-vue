a646debafcbb658532842f8b39bd254a820c165c
Mc Float <marclouie.cabigas@yahoo.com>
2025-05-21 10:16:45 +0800

    cursor-pointer and select-none on default Button

--- apps/v4/registry/new-york-v4/ui/button/Button.vue
+++ apps/v4/registry/new-york-v4/ui/button/Button.vue
@@ -20,7 +20,7 @@ const props = withDefaults(defineProps<Props>(), {
     data-slot="button"
     :as="as"
     :as-child="asChild"
-    :class="cn(buttonVariants({ variant, size }), props.class)"
+    :class="cn('select-none cursor-pointer', buttonVariants({ variant, size }), props.class)"
   >
     <slot />
   </Primitive>
