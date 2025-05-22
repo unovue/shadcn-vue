c5a992fe1f76c5e4a1a71fa05fbd924eab6a05a8
Sadegh Barati <sadeghbaratiwork@gmail.com>
2025-05-22 12:13:28 +0330

    refactor!: remove label props from the `Separator` in tailwindcss v3
    in order to match with the main shadcn-ui

--- apps/www/src/registry/new-york/examples/SeparatorDemo.vue
+++ apps/www/src/registry/new-york/examples/SeparatorDemo.vue
@@ -12,7 +12,7 @@ import { Separator } from '@/registry/new-york/ui/separator'
         An open-source UI component library.
       </p>
     </div>
-    <Separator class="my-4" label="Or" />
+    <Separator class="my-4" />
     <div class="flex h-5 items-center space-x-4 text-sm">
       <div>Blog</div>
       <Separator orientation="vertical" />
