6b4302d87a4512dd4d507a940a34fda3399f7300
Sadegh Barati <sadeghbaratiwork@gmail.com>
2025-05-24 07:36:33 +0330

    chore: remove cjs-to-esm util and test file cause tailwind.config is in css file in v4

--- packages/cli/test/utils/transform-cjs-to-esm.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, it } from 'vitest'
-import { TAILWIND_CONFIG, TAILWIND_CONFIG_WITH_VARIABLES } from '../../src/utils/templates'
-import { transformCJSToESM } from '../../src/utils/transformers/transform-cjs-to-esm'
-
-it('handle tailwind config template correctly', () => {
-  expect(transformCJSToESM('.mjs', TAILWIND_CONFIG)).toMatchSnapshot()
-  expect(transformCJSToESM('.mjs', TAILWIND_CONFIG_WITH_VARIABLES)).toMatchSnapshot()
-})
