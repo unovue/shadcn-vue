26eb3c962e4a40b811f5ceca505063ffc418c682
Menci <mencici@msn.com>
2025-06-24 22:35:32 +0800

    Update Sidebar.vue

--- apps/www/src/registry/default/ui/sidebar/Sidebar.vue
+++ apps/www/src/registry/default/ui/sidebar/Sidebar.vue
@@ -68,7 +68,7 @@ const { isMobile, state, openMobile, setOpenMobile } = useSidebar()
           : 'right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]',
         // Adjust the padding for floating and inset variants.
         variant === 'floating' || variant === 'inset'
-          ? 'p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)_+_theme(spacing.4)_+2px)]'
+          ? 'p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)_+_theme(spacing.4)_+_2px)]'
           : 'group-data-[collapsible=icon]:w-[--sidebar-width-icon] group-data-[side=left]:border-r group-data-[side=right]:border-l',
         props.class,
       )"
