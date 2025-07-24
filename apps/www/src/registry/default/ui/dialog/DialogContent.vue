d8627228303fdc44db04c35b2df01e9fc495b028
Benjamin Oddou <85166574+BenjaminOddou@users.noreply.github.com>
2025-07-24 19:05:03 +0200

    fix: align close button of www/src/registry/default/ui/dialog/DialogContent.vue

--- apps/www/src/registry/default/ui/dialog/DialogContent.vue
+++ apps/www/src/registry/default/ui/dialog/DialogContent.vue
@@ -37,7 +37,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
       <slot />
 
       <DialogClose
-        class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
+        class="absolute right-3 top-3 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
       >
         <X class="w-4 h-4" />
         <span class="sr-only">Close</span>
