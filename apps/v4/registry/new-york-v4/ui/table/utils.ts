4c9531f9bcbf9affaee3202b5d074551c1d099bd
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2025-08-01 13:46:38 +0200

    refactor (table): improves type-safety for valueUpdater

--- apps/v4/registry/new-york-v4/ui/table/utils.ts
+++ apps/v4/registry/new-york-v4/ui/table/utils.ts
@@ -1,9 +1,10 @@
+import { isFunction } from '@tanstack/vue-table';
+
 import type { Updater } from '@tanstack/vue-table'
 import type { Ref } from 'vue'
 
-export function valueUpdater<T extends Updater<any>>(updaterOrValue: T, ref: Ref) {
-  ref.value
-    = typeof updaterOrValue === 'function'
-      ? updaterOrValue(ref.value)
-      : updaterOrValue
+export function valueUpdater<T>(updaterOrValue: Updater<T>, ref: Ref<T>) {
+  ref.value = isFunction(updaterOrValue)
+    ? updaterOrValue(ref.value)
+    : updaterOrValue;
 }
