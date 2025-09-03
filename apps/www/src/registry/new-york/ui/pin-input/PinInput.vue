ed865ddbe101d7101538ad428ce4806673799037
Sadegh Barati <sadeghbaratiwork@gmail.com>
2025-09-03 19:24:34 +0330

    fix(pin-input): enhance type safety by adding generic type support for v3 registry

--- apps/www/src/registry/new-york/ui/pin-input/PinInput.vue
+++ apps/www/src/registry/new-york/ui/pin-input/PinInput.vue
@@ -1,14 +1,14 @@
-<script setup lang="ts">
+<script setup lang="ts" generic="Type extends 'text' | 'number' = 'text'">
 import type { PinInputRootEmits, PinInputRootProps } from "reka-ui"
 import type { HTMLAttributes } from "vue"
 import { reactiveOmit } from "@vueuse/core"
 import { PinInputRoot, useForwardPropsEmits } from "reka-ui"
 import { cn } from "@/lib/utils"
 
-const props = withDefaults(defineProps<PinInputRootProps & { class?: HTMLAttributes["class"] }>(), {
+const props = withDefaults(defineProps<PinInputRootProps<Type> & { class?: HTMLAttributes["class"] }>(), {
   modelValue: () => [],
 })
-const emits = defineEmits<PinInputRootEmits>()
+const emits = defineEmits<PinInputRootEmits<Type>>()
 
 const delegatedProps = reactiveOmit(props, "class")
 
