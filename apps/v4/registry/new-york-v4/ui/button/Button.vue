d5d1bee79f8bb62f0ffafc68098dcda80c30f6d2
David Fournier <david@segments.ai>
2025-09-11 10:18:23 +0400

    feat(button): declare disabled prop

--- apps/v4/registry/new-york-v4/ui/button/Button.vue
+++ apps/v4/registry/new-york-v4/ui/button/Button.vue
@@ -10,6 +10,7 @@ interface Props extends PrimitiveProps {
   variant?: ButtonVariants["variant"]
   size?: ButtonVariants["size"]
   class?: HTMLAttributes["class"]
+  disabled?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -23,6 +24,7 @@ const props = withDefaults(defineProps<Props>(), {
     :as="as"
     :as-child="asChild"
     :class="cn(buttonVariants({ variant, size }), props.class)"
+    :disabled="disabled"
   >
     <slot />
   </Primitive>
--- apps/www/src/registry/new-york/ui/button/Button.vue
+++ apps/www/src/registry/new-york/ui/button/Button.vue
@@ -10,6 +10,7 @@ interface Props extends PrimitiveProps {
   variant?: ButtonVariants["variant"]
   size?: ButtonVariants["size"]
   class?: HTMLAttributes["class"]
+  disabled?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -22,6 +23,7 @@ const props = withDefaults(defineProps<Props>(), {
     :as="as"
     :as-child="asChild"
     :class="cn(buttonVariants({ variant, size }), props.class)"
+    :disabled="disabled"
   >
     <slot />
   </Primitive>
