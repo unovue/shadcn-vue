ac634a65cfc4087a710842e45c564339369f863c
Sadegh Barati <sadeghbaratiwork@gmail.com>
2025-09-23 09:15:08 +0330

    fix: remove fieldItemContext check cause FormItem is always in the form Anatomy

--- apps/www/src/registry/new-york/ui/form/useFormField.ts
+++ apps/www/src/registry/new-york/ui/form/useFormField.ts
@@ -6,8 +6,8 @@ export function useFormField() {
   const fieldContext = inject(FieldContextKey)
   const fieldItemContext = inject(FORM_ITEM_INJECTION_KEY)
 
-  if (!fieldContext || !fieldItemContext)
-    throw new Error("useFormField should be used within <FormField> and <FormItem>")
+  if (!fieldContext)
+    throw new Error("useFormField should be used within <FormField>")
 
   const { name, errorMessage: error, meta } = fieldContext
   const id = fieldItemContext
