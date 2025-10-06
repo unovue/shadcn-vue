d4cfbfe179e405d16964b2fb6c0cb07a4129b628
Sadegh Barati <sadeghbaratiwork@gmail.com>
2025-10-06 09:47:21 +0330

    fix(cli): replace `ensureDependencyInstalled` with  `addDependency`
    cause it not install dependencies properly

--- packages/cli/src/utils/updaters/update-dependencies.ts
+++ packages/cli/src/utils/updaters/update-dependencies.ts
@@ -1,6 +1,6 @@
 import type { RegistryItem } from '@/src/schema'
 import type { Config } from '@/src/utils/get-config'
-import { ensureDependencyInstalled } from 'nypm'
+import { addDependency } from 'nypm'
 import { spinner } from '@/src/utils/spinner'
 
 export async function updateDependencies(
@@ -27,26 +27,20 @@ export async function updateDependencies(
   dependenciesSpinner?.start()
 
   if (dependencies?.length) {
-    const ensureDeps = dependencies.map(dep => ensureDependencyInstalled(dep, {
+    await addDependency(dependencies, {
       cwd: config.resolvedPaths.cwd,
-      // @ts-expect-error type error
       silent: true,
       dev: false,
-    }))
-
-    await Promise.all(ensureDeps)
+    })
   }
 
   // Install dev dependencies
   if (devDependencies?.length) {
-    const ensureDevDeps = devDependencies.map(dep => ensureDependencyInstalled(dep, {
+    await addDependency(devDependencies, {
       cwd: config.resolvedPaths.cwd,
-      // @ts-expect-error type error
       silent: true,
       dev: true,
-    }))
-
-    await Promise.all(ensureDevDeps)
+    })
   }
 
   dependenciesSpinner?.succeed()
