28364c8a607121eaf00a676a212ed1731b49fcdb
Julian Schramm <hi@julian.center>
2025-10-12 00:45:55 +0200

    feat(enhancement): add custom error page
    chore: adjust component structure

---
+++ apps/v4/components/SiteBody.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { config, isLayoutFull } = useConfig()
+const activeTheme = computed(() => config.value.activeTheme)
+const isScaled = computed(() => !!activeTheme.value?.endsWith('-scaled'))
+</script>
+
+<template>
+  <Body
+    class="text-foreground group/body overscroll-none font-sans antialiased [--footer-height:calc(var(--spacing)*14)] [--header-height:calc(var(--spacing)*14)] xl:[--footer-height:calc(var(--spacing)*24)]"
+    :class="[
+      activeTheme ? `theme-${activeTheme}` : '',
+      isScaled ? 'theme-scaled' : '',
+      isLayoutFull ? 'layout-full' : 'layout-fixed',
+    ]"
+  >
+    <slot />
+  </Body>
+</template>
