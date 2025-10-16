d0dd363b61d33cabd38c7de261ed9acde9eb422e
Hooray <304327508@qq.com>
2025-10-16 15:58:45 +0800

    style(InputGroup): textarea wrong style

--- apps/www/src/registry/default/ui/input-group/InputGroupTextarea.vue
+++ apps/www/src/registry/default/ui/input-group/InputGroupTextarea.vue
@@ -12,7 +12,7 @@ const props = defineProps<{
   <Textarea
     data-slot="input-group-control"
     :class="cn(
-      'flex-1 resize-none rounded-none border-0 bg-transparent py-3 shadow-none focus-visible:ring-0 dark:bg-transparent',
+      'flex-1 resize-none rounded-none border-0 bg-transparent py-3 shadow-none focus-visible:ring-0 focus-visible:ring-transparent ring-offset-transparent dark:bg-transparent',
       props.class,
     )"
   />
