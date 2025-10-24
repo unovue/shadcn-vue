c4869e90ddb1ceda222f9f6d6a8f4aae35ad2123
zernonia <zernonia@gmail.com>
2025-10-24 11:31:49 +0800

    chore: prepare cf deploy

--- apps/v4/nuxt.config.ts
+++ apps/v4/nuxt.config.ts
@@ -7,11 +7,15 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css', 'vue-sonner/style.css'],
   modules: ['@nuxtjs/color-mode', '@nuxt/fonts', '@nuxt/content', 'nuxt-shiki'],
   components: [
-    { path: '~/components' },
-    { path: '~/components/_internal', enabled: false },
+    { path: '~/components', ignore: ['_internal/*', '_internal/**/*'] },
     { path: '~/components/demo', pathPrefix: false },
     { path: '~/components/content', global: true, pathPrefix: false },
-    { path: '~/registry/new-york-v4/ui/accordion', global: true, pathPrefix: false, ignore: ['*.ts'] },
+    {
+      path: '~/registry/new-york-v4/ui/accordion',
+      global: true,
+      pathPrefix: false,
+      ignore: ['*.ts'],
+    },
   ],
   fonts: {
     defaults: {
@@ -28,27 +32,36 @@ export default defineNuxtConfig({
       type: 'd1',
       bindingName: 'DB',
     },
+    // required to prevent error related to better-sqlite3 during build and deploy
+    experimental: {
+      sqliteConnector: 'native',
+    },
   },
   shiki: {
     defaultTheme: {
       light: 'github-light-default',
       dark: 'github-dark',
     },
-    bundledLangs: ['ts', 'tsx', 'js', 'vue', 'html', 'json', 'bash', 'astro', 'toml'],
+    bundledLangs: [
+      'ts',
+      'tsx',
+      'js',
+      'vue',
+      'html',
+      'json',
+      'bash',
+      'astro',
+      'toml',
+    ],
   },
   vite: {
-    plugins: [
-      tailwindcss() as any,
-    ],
+    plugins: [tailwindcss()],
   },
   colorMode: {
     classSuffix: '',
   },
   build: {
-    transpile: [
-      'vee-validate',
-      'vue-sonner',
-    ],
+    transpile: ['vee-validate', 'vue-sonner'],
   },
   nitro: {
     preset: 'cloudflare-module',
@@ -57,17 +70,13 @@ export default defineNuxtConfig({
       nodeCompat: true,
       wrangler: {
         name: 'shadcn-vue-nuxt',
-        d1_databases: [{ binding: 'DB', database_id: '4c26cb33-9277-4c9b-8433-42f0a6e84b69' }],
+        d1_databases: [
+          {
+            binding: 'DB',
+            database_id: '4c26cb33-9277-4c9b-8433-42f0a6e84b69',
+          },
+        ],
       },
     },
-    prerender: {
-      // Pre-render the homepage
-      routes: ['/'],
-      // Then crawl all the links on the page
-      // crawlLinks: true,
-    },
-  },
-  routeRules: {
-    '/**': { static: true },
   },
 })
