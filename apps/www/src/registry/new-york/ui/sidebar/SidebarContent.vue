86fb1ca14b5dfca150ea461c4a523e56bce8d687
wankong <1549072621@qq.com>
2025-10-28 09:34:00 +0800

    fix(sidebar): correct overflow for collapsible icon

--- apps/www/src/registry/new-york/ui/sidebar/SidebarContent.vue
+++ apps/www/src/registry/new-york/ui/sidebar/SidebarContent.vue
@@ -10,7 +10,7 @@ const props = defineProps<{
 <template>
   <div
     data-sidebar="content"
-    :class="cn('flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden', props.class)"
+    :class="cn('flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-x-hidden', props.class)"
   >
     <slot />
   </div>
