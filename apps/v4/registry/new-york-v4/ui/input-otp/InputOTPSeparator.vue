68b7476ec88aac42b8ba3b9e150c5d8f5a0f91b5
Julian Schramm <hi@julian.center>
2025-11-02 03:18:54 +0100

    feat(v4): new implementation for the `InputOTP` component (#1505)
    * feat: reimplement `InputOTP` component using vue-input-otp
    
    * docs: update `InputOTP` component documentation, add examples
    
    * fix(docs): correct manual installation step for input otp
    
    * feat: wrap InputOTPSeparator icon with slot so the user could change the icon easily
    
    * chore: update demos and use componentField instead of field slotProps
    
    componentField contains v-model binding
    
    * feat: update tp vue-input-otp 0.3.0
    
    ---------
    
    Co-authored-by: Sadegh Barati <sadeghbaratiwork@gmail.com>

---
+++ apps/v4/registry/new-york-v4/ui/input-otp/InputOTPSeparator.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue"
+import { MinusIcon } from "lucide-vue-next"
+import { useForwardProps } from "reka-ui"
+
+const props = defineProps<{ class?: HTMLAttributes["class"] }>()
+
+const forwarded = useForwardProps(props)
+</script>
+
+<template>
+  <div
+    data-slot="input-otp-separator"
+    role="separator"
+    v-bind="forwarded"
+  >
+    <slot>
+      <MinusIcon />
+    </slot>
+  </div>
+</template>
