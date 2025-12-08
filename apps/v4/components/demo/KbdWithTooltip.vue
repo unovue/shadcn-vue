d1412b233f050256ebb6999c79c2ae3809c270a2
Jevin <jevin98@163.com>
2025-12-08 09:25:57 +0800

    chore: other

--- apps/v4/components/demo/KbdWithTooltip.vue
+++ apps/v4/components/demo/KbdWithTooltip.vue
@@ -5,6 +5,7 @@ import { Kbd, KbdGroup } from '@/registry/new-york-v4/ui/kbd'
 import {
   Tooltip,
   TooltipContent,
+  TooltipProvider,
   TooltipTrigger,
 } from '@/registry/new-york-v4/ui/tooltip'
 </script>
@@ -12,34 +13,38 @@ import {
 <template>
   <div class="flex flex-wrap gap-4">
     <ButtonGroup>
-      <Tooltip>
-        <TooltipTrigger as-child>
-          <Button size="sm" variant="outline">
-            Save
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent>
-          <div class="flex items-center gap-2">
-            Save Changes <Kbd>S</Kbd>
-          </div>
-        </TooltipContent>
-      </Tooltip>
-      <Tooltip>
-        <TooltipTrigger as-child>
-          <Button size="sm" variant="outline">
-            Print
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent>
-          <div class="flex items-center gap-2">
-            Print Document
-            <KbdGroup>
-              <Kbd>Ctrl</Kbd>
-              <Kbd>P</Kbd>
-            </KbdGroup>
-          </div>
-        </TooltipContent>
-      </Tooltip>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger as-child>
+            <Button size="sm" variant="outline">
+              Save
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <div class="flex items-center gap-2">
+              Save Changes <Kbd>S</Kbd>
+            </div>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger as-child>
+            <Button size="sm" variant="outline">
+              Print
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <div class="flex items-center gap-2">
+              Print Document
+              <KbdGroup>
+                <Kbd>Ctrl</Kbd>
+                <Kbd>P</Kbd>
+              </KbdGroup>
+            </div>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </ButtonGroup>
   </div>
 </template>
