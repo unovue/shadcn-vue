6cea4965edc3c284cc0f697ffcb01e9883df2ed8
Jevin <jevin98@163.com>
2025-12-12 14:49:43 +0800

    fix(resizable-panel): expose splitterPanel ref

--- apps/v4/registry/new-york-v4/ui/resizable/ResizablePanel.vue
+++ apps/v4/registry/new-york-v4/ui/resizable/ResizablePanel.vue
@@ -1,19 +1,33 @@
 <script setup lang="ts">
 import type { SplitterPanelEmits, SplitterPanelProps } from "reka-ui"
-import { SplitterPanel, useForwardExpose, useForwardPropsEmits } from "reka-ui"
+import { SplitterPanel, useForwardPropsEmits } from "reka-ui"
 
 const props = defineProps<SplitterPanelProps>()
 const emits = defineEmits<SplitterPanelEmits>()
 
 const forwarded = useForwardPropsEmits(props, emits)
-useForwardExpose()
+const SplitterPanelRef = useTemplateRef<InstanceType<typeof SplitterPanel>>("SplitterPanelRef")
+
+defineExpose({
+  collapse: () => SplitterPanelRef.value?.collapse(),
+  expand: () => SplitterPanelRef.value?.expand(),
+  getSize: () => SplitterPanelRef.value?.getSize(),
+  resize: (size: number) => SplitterPanelRef.value?.resize(size),
+  get isCollapsed() {
+    return SplitterPanelRef.value?.isCollapsed
+  },
+  get isExpanded() {
+    return SplitterPanelRef.value?.isExpanded
+  },
+})
 </script>
 
 <template>
   <SplitterPanel
     v-slot="slotProps"
-    data-slot="resizable-panel"
     v-bind="forwarded"
+    ref="SplitterPanelRef"
+    data-slot="resizable-panel"
   >
     <slot v-bind="slotProps" />
   </SplitterPanel>
