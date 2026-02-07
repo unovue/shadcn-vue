43e66e8d7c42e6ccac0d735c66755ee11079093e
Michał <mwargan@gmail.com>
2026-02-07 14:17:50 +0100

    Update CommandList.vue to adjust max-width

--- apps/v4/registry/new-york-v4/ui/command/CommandList.vue
+++ apps/v4/registry/new-york-v4/ui/command/CommandList.vue
@@ -16,7 +16,7 @@ const forwarded = useForwardProps(delegatedProps)
   <ListboxContent
     data-slot="command-list"
     v-bind="forwarded"
-    :class="cn('max-h-[300px] scroll-py-1 overflow-x-hidden overflow-y-auto', props.class)"
+    :class="cn('max-h-[300px] scroll-py-1 overflow-x-hidden overflow-y-auto max-w-(--reka-popper-available-width)', props.class)"
   >
     <div role="presentation">
       <slot />
