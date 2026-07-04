c0f8f5a1c172340b6c16b7a515c0ec35a0890ff0
Whbbit1999 <60510247+Whbbit1999@users.noreply.github.com>
2026-07-04 19:25:25 +0800

    fix: change the cn-xxx entries in new-york-v4 to the correct tailwind class names #1863 #1861 (#1867)

--- apps/v4/registry/new-york-v4/ui/marker/MarkerContent.vue
+++ apps/v4/registry/new-york-v4/ui/marker/MarkerContent.vue
@@ -12,7 +12,7 @@ const props = defineProps<Props>()
 <template>
   <span
     data-slot="marker-content"
-    :class="cn('cn-marker-content min-w-0 wrap-break-word', props.class)"
+    :class="cn('group-data-[variant=separator]/marker:flex-none group-data-[variant=separator]/marker:text-center *:[a]:hover:text-foreground *:[a]:underline *:[a]:underline-offset-3 min-w-0 wrap-break-word', props.class)"
   >
     <slot />
   </span>
